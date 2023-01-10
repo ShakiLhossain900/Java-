fb3092143c1eb1a49f5c6e029077cec9ba507252
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2023-01-10 22:53:46 +0600

    Add files via upload

---
+++ Java programming  for springg 2023/W3schoolPractice/src/main/java/com/mycompany/w3schoolpractice/DoWhileLoop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.w3schoolpractice;
+
+/**
+ *
+ * @author Itanium
+ */
+public class DoWhileLoop {
+    
+    public static void main (String[] shakil){
+        int i=0;
+        while(i<5){
+            System.out.println(i);
+          i++;
+        }
+    }
+}
